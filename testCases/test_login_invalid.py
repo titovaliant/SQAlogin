67a66016bc21272bd26864e0ef097f6dfbf32687
TITO VALIANT M.F <54594147+titovaliant@users.noreply.github.com>
2022-06-10 11:54:41 +0700

    Add files via upload

--- testCases/test_login_invalid.py
+++ testCases/test_login_invalid.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from utilities.dataLog import Log_Data
 from utilities import excel
-
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import pytest
 import time
@@ -19,7 +19,9 @@ def driver():
         yield driver
         driver.quit()
 
-def readData():
+log = Log_Data.custom_logger()
+
+def readDataInvalidEmail():
         list = []
         path='.//TestData/LoginData_invalid.xlsx'
         
@@ -28,34 +30,40 @@ def readData():
         # sheet = workbook.get_sheet_by_name("Sheet1")
 
         # rows = sheet.max_row
-        rows = excel.getRowCount(path, 'Sheet1')
+        # rows = excel.getRowCount(path, 'Sheet1')
 
-        for r in range(2, rows+1):
-            username=excel.readData(path, 'Sheet1',r,1)
-            password=excel.readData(path, 'Sheet1',r,2)
-            # username = sheet.cell(r, 1).value
-            # password = sheet.cell(r, 2).value
-
+        for r1 in range(2, 4):
+            username=excel.readData(path, 'Sheet1',r1,1)
+            password=excel.readData(path, 'Sheet1',r1,2)
             tuple = (username,password)
             list.append(tuple)
 
         print(list)
         return list
-        
-log = Log_Data.custom_logger()
 
 def readDataEmailEmpty():
         list = []
         path='.//TestData/LoginData_invalid.xlsx'
-    
-        rows = excel.getRowCount(path, 'Sheet1')
 
-        for r in range(2, rows+1):
-            username=excel.readData(path, 'Sheet1',r,4)
-            password=excel.readData(path, 'Sheet1',r,5)
+        for r2 in range(2, 4):
+            username=excel.readData(path, 'Sheet1',r2,4)
+            password=excel.readData(path, 'Sheet1',r2,5)
             # username = sheet.cell(r, 1).value
             # password = sheet.cell(r, 2).value
+            tuple = (username,password)
+            list.append(tuple)
 
+        print(list)
+        return list
+
+def readDataInvalidPassword():
+        list = []
+        path='.//TestData/LoginData_invalid.xlsx'
+        rows = excel.getRowCount(path, 'Sheet1')
+
+        for r3 in range(8, rows+1):
+            username=excel.readData(path, 'Sheet1',r3,1)
+            password=excel.readData(path, 'Sheet1',r3,2)
             tuple = (username,password)
             list.append(tuple)
 
@@ -65,23 +73,111 @@ def readDataEmailEmpty():
 #===============================================================================================
 # TESTCASE INVALID 1 : LOGIN with wrong email format (exp: @gamil.com) and unregistered email
 #===============================================================================================
-@pytest.mark.parametrize("username,password", readData())
-def test_invalidEmail_login(driver, username, password): #DataDrivenTest
+@pytest.mark.parametrize("username,password", readDataInvalidEmail())
+def test1_invalidEmail_login(driver, username, password): #DataDrivenTest
         
-        log.info("=== Test_3_invalid_Login_EMAIL ===")
+        log.info("=== Test_1_invalid_Login_wrong format EMAIL ===")
         driver.find_element(By.ID, "input_email").send_keys(username)
 
         driver.find_element(By.ID, "input_password").send_keys(password)
         driver.find_element(By.ID, "submit_login").click()
-        # time.sleep(4)
+        time.sleep(1)
 
         check = driver.find_element(By.ID, "input_email-helper-text").text
         if check == "Email yang anda masukan salah atau tidak terdaftar":
             assert True
-            log.info("=== test 3_invalid Login Email Test: PASS")
+            log.info("=== test 1_invalid wrong format Email Login Test: PASS")
         else:
-            log.error("=== test 3_invalid Login Email Test: FAIL")
-            driver.save_screenshot(".\\Screenshots\\" + "test_login_invalid_email.png")
+            log.error("=== test 1_invalid wrong format Email Login Test: FAIL")
+            driver.save_screenshot(".\\Screenshots\\" + "test1_login_invalid_wrong_email.png")
+            assert False
+
+#===============================================================================================
+# TESTCASE INVALID 2 : LOGIN by leaving email field empty
+#===============================================================================================
+@pytest.mark.parametrize("password", readDataEmailEmpty())
+def test2_invalidEmpty_Email_login(driver, password): #DataDrivenTest
+        
+        log.info("=== Test_2_invalid_Login_empty EMAIL ===")
+        driver.find_element(By.ID, "input_email").click()
+        driver.find_element(By.ID, "input_email").send_keys(Keys.TAB)
+        time.sleep(1)
+        driver.find_element(By.ID, "input_password").send_keys(password)
+        driver.find_element(By.ID, "submit_login").click()
+
+        check = driver.find_element(By.ID, "input_email-helper-text").text
+        time.sleep(1)
+        if check == "Email yang anda masukan salah atau tidak terdaftar":
+            if "Password yang anda masukan salah" in driver.find_element(By.ID, "input_password-helper-text").text:
+                assert True
+                log.info("=== test 2_invalid empty Email Login Test: PASS")
+        else:
+            log.error("=== test 2_invalid empty Email Login Test: FAIL")
+            driver.save_screenshot(".\\Screenshots\\" + "test2_login_invalid_empty_email.png")
+            assert False
+
+#===============================================================================================
+# TESTCASE INVALID 3 : LOGIN by leaving password field empty
+#===============================================================================================
+@pytest.mark.parametrize("username", readDataInvalidPassword())
+def test3_invalidEmpty_Password_login(driver, username): #DataDrivenTest
+        
+        log.info("=== Test_3_invalid_Login_empty PASSWORD ===")
+        driver.find_element(By.ID, "input_email").send_keys(username)
+        driver.find_element(By.ID, "submit_login").click()
+        time.sleep(1)
+        check = driver.find_element(By.ID, "input_email-helper-text").text
+        
+        if check == "Email yang anda masukan salah atau tidak terdaftar":
+            if "Password yang anda masukan salah" in driver.find_element(By.ID, "input_password-helper-text").text:
+                assert True
+                log.info("=== test 3_invalid empty PASSWORD Login Test: PASS")
+        else:
+            log.error("=== test 3_invalid empty PASSWORD Login Test: FAIL")
+            driver.save_screenshot(".\\Screenshots\\" + "test3_login_invalid_empty_password.png")
+            assert False
+
+#===============================================================================================
+# TESTCASE INVALID 4 : LOGIN by leaving email field & password field empty
+#===============================================================================================
+def test4_invalidEmpty_login(driver):
+        
+        log.info("=== Test_4_invalid_Login_empty EMAIL & PASSWORD ===")
+        driver.find_element(By.ID, "input_email").click()
+        driver.find_element(By.ID, "input_password").click()
+        driver.find_element(By.ID, "submit_login").click()
+        time.sleep(1)
+        check = driver.find_element(By.ID, "input_email-helper-text").text
+        
+        if check == "Email yang anda masukan salah atau tidak terdaftar":
+            if "Password yang anda masukan salah" in driver.find_element(By.ID, "input_password-helper-text").text:
+                assert True
+                log.info("=== Test 4_invalid_Login_empty EMAIL & PASSWORD: PASS")
+        else:
+            log.error("=== Test 4_invalid_Login_empty EMAIL & PASSWORD: FAIL")
+            driver.save_screenshot(".\\Screenshots\\" + "test4_login_invalid_empty_EmailPassword.png")
+            assert False
+
+#===============================================================================================
+# TESTCASE INVALID 5 : LOGIN with valid email and wrong/unvalid password
+#===============================================================================================
+@pytest.mark.parametrize("username,password", readDataInvalidPassword())
+def test5_invalidPassword_login(driver, username, password): #DataDrivenTest
+        
+        log.info("=== Test_5_invalid_Login_PASSWORD ===")
+        driver.find_element(By.ID, "input_email").send_keys(username)
+
+        driver.find_element(By.ID, "input_password").send_keys(password)
+        driver.find_element(By.ID, "submit_login").click()
+        time.sleep(1)
+
+        check = driver.find_element(By.ID, "input_password-helper-text").text
+        if check == "Password yang anda masukan salah":
+            assert True
+            log.info("=== test 5_invalid_Login_PASSWORD: PASS")
+        else:
+            log.error("=== test 5_invalid_Login_PASSWORD: FAIL")
+            driver.save_screenshot(".\\Screenshots\\" + "test5_invalid_Login_PASSWORD.png")
             assert False
 
         
--- testCases/test_login_valid.py
+++ testCases/test_login_valid.py
@@ -20,7 +20,7 @@ class Test_valid_Login:
 #===============================================================================
 
     def test1_loginPageTitle(self):
-        self.log.info("=== Test_1_Title_Login ===")
+        self.log.info("=== Test_1_valid_Title_Login ===")
         
         self.driver = webdriver.Chrome()
         self.driver.get(self.baseURL)
@@ -30,12 +30,11 @@ class Test_valid_Login:
         if get_title=='Populix':
             assert True
             self.log.info("=== test 1_Login Page Title: PASS")
-            self.driver.close()
         else:
             self.log.error("=== test 1_Login Page Title: FAIL")
-            self.driver.close()
-            self.driver.save_screenshot(".\\Screenshots\\" + "test_loginTitle.png")
+            self.driver.save_screenshot(".\\Screenshots\\" + "test 1_loginTitle.png")
             assert False
+        self.driver.quit()
 
 #===============================================================================
 # TESTCASE 2 : LOGIN using a valid email & valid password
@@ -46,82 +45,88 @@ class Test_valid_Login:
         self.driver = webdriver.Chrome()
         self.driver.get(self.baseURL)
         self.lp = LoginPages(self.driver)
-        time.sleep(1)
         self.lp.setUserName(self.username)
         self.lp.setPassword(self.password)
         self.lp.clickLogin()
      
-        time.sleep(3)
+        time.sleep(5)
         page_url = self.driver.current_url
         if page_url=='https://www.populix.co/study':
-            if self.driver.find_element(By.ID, "topnav-btn-avatar").is_displayed():
+            if self.driver.find_element(By.CLASS_NAME, "sidebar-img-profile").is_displayed():
                 assert True
                 self.log.info("=== test 2_Valid Login Test: PASS")
-                self.driver.close()
         else:
             self.log.error("=== test 2_Valid Login Test: FAIL")
-            self.driver.save_screenshot(".\\Screenshots\\" + "test_login_valid.png")
-            self.driver.close()
+            self.driver.save_screenshot(".\\Screenshots\\" + "test 2_login_valid.png")
             assert False 
+        self.driver.quit()
 
-#=========================================================================================
-# TESTCASE 3 : LOGIN by pressing the Enter key with a valid email and password
-#=========================================================================================
+#===============================================================================
+# TESTCASE 3 : Verify clicking on the eye icon to show the password
+#===============================================================================
 
-    def test3_login_valid_enter_key(self):
-        self.log.info("=== Test_3_valid_Login ===")
-
+    def test3_password_eye_icon(self):
+        self.log.info("=== Test_3_valid_password_eye_icon ===")
         self.driver = webdriver.Chrome()
         self.driver.get(self.baseURL)
         self.lp = LoginPages(self.driver)
-
+        time.sleep(2)
         self.lp.setUserName(self.username)
-        self.lp.setPassword(self.password)
-        self.lp.enterLogin()
-
-        time.sleep(3)
-        page_url = self.driver.current_url
-        if page_url=='https://www.populix.co/study':
-            if self.driver.find_element(By.ID, "topnav-btn-avatar").is_displayed():
-                assert True
-                self.log.info("=== test 3_Valid enterLogin Test: PASS")
-                self.driver.close()
-        else:
-            self.log.error("=== test 3_Valid enterLogin Test: FAIL")
-            self.driver.save_screenshot(".\\Screenshots\\" + "test_login_valid.png")
-            self.driver.close()
-            assert False 
-
-#===============================================================================
-# TESTCASE 4 : Verify clicking on the eye icon to show the password
-#===============================================================================
-
-    def test4_password_eye_icon(self):
-        self.log.info("=== Test_4_password_eye_icon ===")
-        self.driver = webdriver.Chrome()
-        self.driver.get(self.baseURL)
-        self.lp = LoginPages(self.driver)
         self.lp.setPassword(self.password)
         
         self.lp.eyePassword()
         time.sleep(2)
-        self.log.info("=== test 4_password eye Test: PASS")
+        self.log.info("=== test 3_password eye Test: PASS")
         self.driver.quit()
+
+#=========================================================================================
+# TESTCASE 4 : LOGIN by pressing the Enter key with a valid email and password
+#=========================================================================================
+
+    def test4_login_valid_enter_key(self):
+        self.log.info("=== Test_4_valid_Login_EnterKey ===")
+
+        self.driver = webdriver.Chrome()
+        self.driver.get(self.baseURL)
+        self.lp = LoginPages(self.driver)
+        time.sleep(2)
+        self.lp.setUserName(self.username)
+        self.lp.setPassword(self.password)
+        self.lp.enterLogin()
+
+        time.sleep(5)
+        page_url = self.driver.current_url
+        if page_url=='https://www.populix.co/study':
+            if self.driver.find_element(By.CLASS_NAME, "sidebar-img-profile").is_displayed():
+                assert True
+                self.log.info("=== Test_4_valid_Login_EnterKey: PASS")
+        else:
+            self.log.error("=== Test_4_valid_Login_EnterKey: FAIL")
+            self.driver.save_screenshot(".\\Screenshots\\" + "test 4_login_valid Enter.png")
+            assert False 
+        self.driver.quit()    
 
 #===================================================================================================
 # TESTCASE 5 : Verify user should be able to move input fields via pressing the TAB key
 #===================================================================================================
 
-    # def test5_valid_tab_key(self):
-    #     self.log.info("=== Test_5_valid TAB key ===")
-    #     self.driver.find_element(By.ID, "input_email").click()
-    #     self.driver.find_element(By.ID, "input_email").send_keys("tito@gmail.com")
-    #     self.driver.find_element(By.ID, "input_email").send_keys(Keys.TAB)
-    #     self.driver.find_element(By.ID, "input_password").send_keys("bingung")
-    #     self.driver.find_element(By.ID, "btn_to-forgot-password").send_keys(Keys.TAB)
-    #     self.driver.find_element(By.ID, "submit_login").send_keys(Keys.TAB)
-    #     self.driver.find_element(By.ID, "login_facebook").click()
-    #     time.sleep(2)
+    def test5_valid_tab_key(self):
+        self.log.info("=== Test_5_valid TAB key ===")
+        
+        self.driver = webdriver.Chrome()
+        self.driver.get(self.baseURL)
+        self.lp = LoginPages(self.driver)
 
-    #     assert 'Facebook' in driver.find_element(By.CLASS_NAME, 'fb_logo').text
-    #     log.info("=== test 5_valid TAB key Test: PASS")
+        self.lp.TABkey(self,username,password)
+        self.lp.clickFBLogin()
+        time.sleep(2)
+
+        check = self.driver.find_element(By.CLASS_NAME, 'fb_logo').text
+        if check=='Facebook':
+            assert True
+            self.log.info("=== Test_5_valid TAB key Test: PASS")
+        else:
+            self.log.error("=== Test_5_valid TAB key Test: PASS")
+            self.driver.save_screenshot(".\\Screenshots\\" + "test 5_login_valid TAB.png")
+            assert False 
+        self.driver.quit()    
